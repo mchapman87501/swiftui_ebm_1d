0af38a612af18fe525832fee7d4440ebf56062e4
Mitch Chapman <mchapman87501@gmail.com>
2019-11-16 15:03:28 -0700

    Initial Commit

---
+++ LearnSwiftUI1/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LearnSwiftUI1
+//
+//  Created by Mitchell Chapman on 11/16/19.
+//  Copyright © 2019 Desert Moon Consulting, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
