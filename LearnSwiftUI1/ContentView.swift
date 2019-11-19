b1fdf6c5c4cddf4bde1e55071d12ad276eaf0c12
Mitch Chapman <mchapman87501@gmail.com>
2019-11-18 21:16:11 -0700

    Futzing around trying to learn SwiftUI syntax/semantics.

--- LearnSwiftUI1/ContentView.swift
+++ LearnSwiftUI1/ContentView.swift
@@ -9,9 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var numLatBands = 9.0
+    @State var latHTC = 7.6
+    
+    var floatFmtr: NumberFormatter = {
+        let f = NumberFormatter()
+        f.numberStyle = .decimal
+        return f
+    }()
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        Form {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Lat Bands: 1")
+                    Slider(value: $numLatBands, in: 1...90)
+                        .frame(maxWidth: 240)
+                    Text("90")
+                }
+                HStack {
+                    Text("Lateral Heat:")
+                    TextField("7.6", value: $latHTC, formatter: floatFmtr)
+                        .frame(maxWidth: 40)
+                        .multilineTextAlignment(.trailing)
+                }
+                
+                Rectangle()
+                    .foregroundColor(.white)
+                    .frame(minHeight: 240)
+
+                HStack {
+                    Spacer()
+                    Rectangle()
+                        .frame(minHeight: 128)
+                        .aspectRatio(CGSize(width: 1, height: 1), contentMode: .fit)
+                    Spacer()
+                    Rectangle()
+                        .frame(minHeight: 128)
+                        .aspectRatio(CGSize(width: 1, height: 1), contentMode: .fit)
+                    Spacer()
+                }
+            }
+            .padding()
+        }
     }
 }
 
