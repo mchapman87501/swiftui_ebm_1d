08ee12195f067f95369f7fc5bad65507e9184b99
Mitch Chapman <mchapman87501@gmail.com>
2020-02-09 08:59:24 -0700

    Abort abort abort

--- EBM1D/Views/ChartViews/AxisLimits.swift
+++ EBM1D/Views/ChartViews/AxisLimits.swift
@@ -12,21 +12,25 @@ struct AxisLimits {
     let axMin: CGFloat
     let axMax: CGFloat
     let extent: CGFloat
+    let ticks: [CGFloat]
     
     init(_ minVal: CGFloat, _ maxVal: CGFloat) {
         guard minVal < maxVal else {
             axMin = 0.0
             axMax = 1.0
             extent = 1.0
+            ticks = [0.0, 1.0]
             return
         }
         let vRange = maxVal - minVal
         let fifths = Self.getFifths(vRange)
         let lo = Self.roundedToTick(minVal, fifths, false)
         let hi = Self.roundedToTick(maxVal, fifths, true)
+        let tks = Self.ticks(lo, hi, fifths * 2.0)
         axMin = lo
         axMax = hi
         extent = hi - lo
+        ticks = tks
     }
     
     private static func getFifths(_ v: CGFloat) -> CGFloat {
@@ -40,6 +44,10 @@ struct AxisLimits {
             vWhole += roundUp ? fifths : -fifths
         }
         return vWhole
+    }
+    
+    private static func ticks(_ lo: CGFloat, _ hi: CGFloat, _ step: CGFloat) -> [CGFloat] {
+        return stride(from: lo, to: hi + step, by: step).map { $0 }
     }
 }
 
